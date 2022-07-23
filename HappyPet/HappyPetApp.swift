43a6b15976435b76eddc8ef52d46e15f7ffa38ab
VictorZima <victorzima@icloud.com>
2022-07-23 05:38:09 +0300

    Initial Commit

---
+++ HappyPet/HappyPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyPetApp.swift
+//  HappyPet
+//
+//  Created by VictorZima on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct HappyPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
