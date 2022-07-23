a3e84adbb6160157c49fc4534c6e775fcda38c65
VictorZima <victorzima@icloud.com>
2022-07-24 02:48:49 +0300

    Refactoring - another solution for logo

--- HappyPet/SplashView.swift
+++ HappyPet/SplashView.swift
@@ -9,67 +9,37 @@ import SwiftUI
 
 struct SplashView: View {
     @State private var showAnimation = false
-    @State private var progress = 1.0
+    let animals: [String] = ["hare", "tortoise", "ladybug", "leaf"]
     
     var body: some View {
         VStack {
+            Spacer()
             HStack(spacing: 10) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .trim(from: 0, to: progress)
-                        .stroke(Color.green, style: StrokeStyle(lineWidth: 4, lineCap: CGLineCap.round))
-                        .frame(width: 70, height: 70)
-                    
-                    Image(systemName: "hare")
-                        .font(.system(size: 33))
-                        .foregroundColor(.black)
+                ForEach(animals, id: \.self) { animal in
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.green, style: StrokeStyle(lineWidth: 4, lineCap: CGLineCap.round))
+                            .frame(width: 70, height: 70)
+                        
+                        Image(systemName: animal)
+                            .font(.system(size: 33))
+                            .foregroundColor(.black)
+                    }
+                    .opacity(showAnimation ? 1 : 0)
+                    .animation(.linear(duration: 1).delay(0.1), value: showAnimation)
                 }
-                .opacity(showAnimation ? 1 : 0)
-                .animation(.linear(duration: 1).delay(0.1), value: showAnimation)
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .trim(from: 0, to: progress)
-                        .stroke(Color.green, style: StrokeStyle(lineWidth: 4, lineCap: CGLineCap.round))
-                        .frame(width: 70, height: 70)
-                    
-                    Image(systemName: "tortoise")
-                        .font(.system(size: 33))
-                        .foregroundColor(.black)
-                }
-                .opacity(showAnimation ? 1 : 0)
-                .animation(.linear(duration: 1).delay(0.2), value: showAnimation)
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .trim(from: 0, to: progress)
-                        .stroke(Color.green, style: StrokeStyle(lineWidth: 4, lineCap: CGLineCap.round))
-                        .frame(width: 70, height: 70)
-                    
-                    Image(systemName: "ladybug")
-                        .font(.system(size: 33))
-                        .foregroundColor(.black)
-                }
-                .opacity(showAnimation ? 1 : 0)
-                .animation(.linear(duration: 1).delay(0.3), value: showAnimation)
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .trim(from: 0, to: progress)
-                        .stroke(Color.green, style: StrokeStyle(lineWidth: 4, lineCap: CGLineCap.round))
-                        .frame(width: 70, height: 70)
-                    
-                    Image(systemName: "hare")
-                        .font(.system(size: 33))
-                        .foregroundColor(.black)
-                }
-                .opacity(showAnimation ? 1 : 0)
-                .animation(.linear(duration: 1).delay(0.4), value: showAnimation)
+            
             }
             Text("HAPPY PET")
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(.black)
+            Spacer()
+            
+            Button("Tap") {
+                self.showAnimation.toggle()
+            }
+            .padding(.bottom, 50)
         }
         .onAppear {
             self.showAnimation.toggle()
